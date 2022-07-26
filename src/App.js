55736eb82269158344c31c69c5ed6916ca7b6538
willianchu <willianchu@hotmail.com>
2022-07-26 15:01:33 -0700

    useState and create props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoList from "./TodoList";
 
 function App() {
+  const [todos, setTodos] = useState(['Task 1', 'Task 2', 'Task 3']);
+
   return (
-    <TodoList />
+    <>
+      <TodoList todos={todos} />
+      <input type="text" />
+      <button>Add</button>
+      <button>Clear Completed</button>
+      <div> left to do</div>
+    </>
   );
 }
 
