5fc72047b588b88773f5488f76b430dc482bedcc
willianchu <willianchu@hotmail.com>
2022-07-27 22:45:17 -0700

    set handle function onchange

--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
-export default function Todo({ todo }) {
+export default function Todo({ todo , toggleTodo }) {
+  function handleTodoClick() {
+    toggleTodo(todo.id)
+  }
   return (
     <div>
       <label>
-        <input type="checkbox" checked={todo.completed} />
+        <input type="checkbox" checked={todo.completed} onChange={handleTodoClick} />
         id {todo.id} - { todo.text }  
       </label>
     </div>
